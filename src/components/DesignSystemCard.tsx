bbbf9b9825500ebc06cda6fef5a6f80953126fb3
Randy Oest <oest@amazingrando.com>
2025-04-28 17:37:48 -0400

    responsive

--- src/components/DesignSystemCard.tsx
+++ src/components/DesignSystemCard.tsx
@@ -14,14 +14,14 @@ export const DesignSystemCard = ({ name, url, institution }: DesignSystem) => {
   return (
     <Card className="flex flex-col h-full bg-yellow-50">
       <CardHeader className="text-green-dark">
-        <h3 className="text-2xl font-semibold">{name}</h3>
-        <p className="text-lg">{institution}</p>
+        <h3 className="text-lg leading-tight md:text-2xl font-semibold">{name}</h3>
+        <p className="text-sm md:text-lg">{institution}</p>
       </CardHeader>
       <CardContent className="flex-grow">
       </CardContent>
       <CardFooter>
         <Button variant="outline" className="w-full bg-yellow-400 ring-yellow-500 ring-1 text-yellow-950 hover:bg-yellow-500" onClick={() => window.open(url, '_blank')}>
-          View Design System
+          View <span className="hidden md:inline">Design System</span>
           <ExternalLink className="w-4 h-4 mr-2" />
         </Button>
       </CardFooter>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,9 +4,8 @@ import { SearchBar } from '@/components/SearchBar';
 import { designSystems } from '@/data/designSystems';
 import bgImage from '@/assets/images/bg.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBuildingColumns,
-} from '@fortawesome/pro-regular-svg-icons';
+import { faBuildingColumns } from '@fortawesome/pro-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { AddYourDS } from '@/components/AddYourDS';
 
 const Index = () => {
@@ -18,20 +17,22 @@ const Index = () => {
   );
 
   return (
-    <div className="min-h-screen bg-green-dark bg-contain bg-no-repeat font-base" style={{ backgroundImage: `url(${bgImage})` }}>
-      <div className="container mx-auto px-8 py-8">
-        <header className="mt-16 mb-12 text-white aspect-[2.7/1] flex gap-8">
-          <FontAwesomeIcon icon={faBuildingColumns} className="text-5xl relative top-1" />
+    <div className="min-h-screen bg-green-dark  bg-position-[center_top_1rem] md:bg-contain bg-no-repeat font-base" style={{ backgroundImage: `url(${bgImage})` }}>
+      <div className="container mx-auto px-4 py-4 md:px-8 md:py-8">
+        
+        <header className="mt-8 md:mt-16 mb-12 text-white md:aspect-[2.7/1] flex gap-4 md:gap-8">
+          <FontAwesomeIcon icon={faBuildingColumns} className="text-3xl md:text-5xl relative top-1" />
+          
           <div className="max-w-2xl">
-            <h1 className="text-6xl text-balance font-bold mb-4">The Design Systems of Higher Education</h1>
-            <p className="text-3xl mb-8 text-balance font-medium">
+            <h1 className="text-3xl md:text-6xl text-balance font-bold mb-4">The Design Systems of Higher Education</h1>
+            <p className="text-lg md:text-3xl mb-8 text-balance font-medium">
               A curated collection of design systems from universities around the world
             </p>
-            <p className="text-lg font-medium">Sponsored by <a href="https://amazingrando.com" target="_blank" rel="noopener noreferrer" className="underline">Amazing Rando Design</a></p>
+            <p className="text-sm md:text-lg font-medium">Sponsored by <a href="https://amazingrando.com" target="_blank" rel="noopener noreferrer" className="underline">Amazing Rando Design</a></p>
           </div>
         </header>
 
-        <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
           {filteredSystems
             .sort((a, b) => a.name.localeCompare(b.name))
             .map((system, index) => (
@@ -41,9 +42,9 @@ const Index = () => {
                   url={system.url}
                   institution={system.institution}
                 />
-                {index === 1 && <div className="md:hidden"><AddYourDS /></div>}
-                {index === 3 && <div className="hidden md:block lg:hidden"><AddYourDS /></div>}
-                {index === 2 && <div className="hidden lg:block"><AddYourDS /></div>}
+                {index === 1 && <div className="md:hidden col-span-2"><AddYourDS /></div>}
+                {index === 3 && <div className="hidden md:block lg:hidden col-span-2"><AddYourDS /></div>}
+                {index === 2 && <div className="hidden lg:block col-span-2"><AddYourDS /></div>}
               </React.Fragment>
             ))}
         </main>
@@ -55,7 +56,7 @@ const Index = () => {
         )}
 
         <footer className="border-t border-yellow-500/20 py-8 text-center text-white mt-8">
-          <p>Build with ❤️ by <a href="https://amazingrando.com" target="_blank" rel="noopener noreferrer" className="underline">Amazing Rando Design</a></p>
+          <p>Build with ❤️ by <a href="https://amazingrando.com" target="_blank" rel="noopener noreferrer" className="underline">Amazing Rando Design</a> • <FontAwesomeIcon icon={faGithub} /> <a href="https://github.com/amazingrando/higher-edu-design-systems" target="_blank" rel="noopener noreferrer" className="underline">View on GitHub</a></p>
         </footer>
       </div>
     </div>
